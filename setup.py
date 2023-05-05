9119557630c7b55882f4a5dba65b90fdb494a40f
asxe <2973918177@qq.com>
2023-05-05 13:12:55 +0800

    修改协议

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version=_version.__version__,
     packages=find_packages(),
     url='https://github.com/asxez/ImageMagic',
-    license='MIT',
+    license='GNU GPL',
     author='asxe',
     author_email='2973918177@qq.com',
     description='图像处理库（包括但不限于图像）【Image processing libraries (including but not limited to images)】',
