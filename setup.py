a500e4f765a94fd1eb749e194ab39a9183f03b6e
asxe <2973918177@qq.com>
2023-05-05 12:37:40 +0800

    修改版本号来源

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from setuptools import setup,find_packages
-
+from ImageMagic import _version
 
 setup(
     name='ImageMagic',
-    version='0.1.5',
+    version=_version.__version__,
     packages=find_packages(),
     url='https://github.com/asxez/ImageMagic',
     license='MIT',
     author='asxe',
     author_email='2973918177@qq.com',
-    description='图像处理库（包括但不限于图像）',
+    description='图像处理库（包括但不限于图像）【Image processing libraries (including but not limited to images)】',
+    long_description='For image processing, it currently supports text to picture,audio to text, and a combination of the two audio content to picture,image format conversion, image classification and OCR functions',
     install_requires=[
         'Pillow',
         'requests',
